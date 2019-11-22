e58654192d4afdb52f9aba4133ef623fbdfa3063
çiğdem işçioğlu <58074630+190303037@users.noreply.github.com>
2019-11-22 16:40:19 +0300

    Create bolunme.py

---
+++ bolunme.py
@@ -0,0 +1,8 @@
+#7 ile bölünebilen ancak 5'in katı olmayan tüm bu sayıları bulabilecek bir program yazın,
+#2000 ila 3200 arasında (her ikisi de dahil).
+#Elde edilen sayılar, tek bir satıra virgülle ayrılmış bir sıra ile basılmalıdır.
+
+i=2000
+while i<3201:
+    if i%7==0 and i%5!=0     
+    i=i+1
